6e0315334abc0b3b7abb87d8b9091bb3d24653a2
Nash0810 <nashco9900@gmail.com>
2025-07-08 21:38:13 +0530

    update

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -14,7 +14,7 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(tokenString, process.env.JWT_SECRET);
 
-    req.user = decoded.user;
+    req.user = decoded;
     next();
   } catch (err) {
     console.error("Token verification failed:", err.message);
